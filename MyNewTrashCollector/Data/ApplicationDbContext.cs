7feb9ebf7d94d6384621f65c4a872ade730b7649
Matthew Alexander <matt.bryantalex@gmail.com>
2021-02-25 14:35:07 -0800

    init

---
+++ MyNewTrashCollector/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyNewTrashCollector.Models;
+
+namespace MyNewTrashCollector.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyNewTrashCollector.Models.Customer> Customer { get; set; }
+    }
+}
---
+++ MyNewTrashCollector/Models/Customer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyNewTrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Customer Name")]
+        public string CustomerName { get; set; }
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Pick Up Day")]
+        public string PickupDay { get; set; }
+        //[Display(Name = "Pick Up Day")]
+        //public bool IsConfirmed { get; set; }
+        [Display(Name = "Amount Owed")]
+        public float AmountOwed { get; set; }
+        [Display(Name = "One Time Pickup Date")]
+        public string OneTimePickupDate { get; set; }
+        [Display(Name = "Start Date for Temporary Pickup")]
+        public string TempSuspendStartDate { get; set; }
+        [Display(Name = "End Date for Temporary Pickup")]
+
+        public string TempSuspendEndDate { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+
+
+
+    }
+}
