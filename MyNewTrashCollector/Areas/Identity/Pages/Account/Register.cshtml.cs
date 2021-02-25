fd669e71cc76234931fe24ddb8d4c9a30734f7ea
Matthew Alexander <matt.bryantalex@gmail.com>
2021-02-25 14:45:46 -0800

    added to onPostAsync

--- MyNewTrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MyNewTrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,7 +88,11 @@ namespace MyNewTrashCollector.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    if (await _roleManager.RoleExistsAsync(Input.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
+                        _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
